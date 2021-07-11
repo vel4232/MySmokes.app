22095fba86d4081058c0093b3182621f7e93055f
Valentin <vel4232@gmail.com>
2021-07-11 23:10:25 +0300

    Initial Commit

---
+++ MySmokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySmokes
+//
+//  Created by Valentin Chetyrbotskiy on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
